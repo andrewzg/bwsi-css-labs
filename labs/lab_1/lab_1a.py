c37a6417e0b8de4503bedb568c554a268ab62501
andrewzg <andrewyangzg@gmail.com>
2026-02-16 12:53:06 -0800

    Personal Introduction

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Andrew Yang" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hello my name is Andrew Yang. I like math, coding, and basketball!")
 
 if __name__ == "__main__":
     main()
